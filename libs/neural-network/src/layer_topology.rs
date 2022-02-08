26468a4046e8494fb0930558abed6ef7dfb029b5
Niclas <niclas.lind93@gmail.com>
2022-02-08 08:41:35 +0100

    add tests

--- libs/neural-network/src/layer_topology.rs
+++ libs/neural-network/src/layer_topology.rs
@@ -2,3 +2,15 @@
 pub struct LayerTopology {
     pub neurons: usize,
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::LayerTopology;
+
+    #[test]
+    fn test() {
+        let layer_topology = LayerTopology { neurons: 3 };
+
+        assert_eq!(3, layer_topology.neurons);
+    }
+}
