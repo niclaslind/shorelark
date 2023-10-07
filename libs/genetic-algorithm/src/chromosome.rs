aefe1dc3d6ac5ecd0ef9ecc4ba7075c2e934b773
Niclas Lind <niclas.lind93@gmail.com>
2023-10-07 13:18:16 +0200

    fix failing build

--- libs/genetic-algorithm/src/chromosome.rs
+++ libs/genetic-algorithm/src/chromosome.rs
@@ -41,8 +41,8 @@ impl FromIterator<f32> for Chromosome {
 
 impl IntoIterator for Chromosome {
     type Item = f32;
-    type IntoIter = impl Iterator<Item = f32>;
-
+    type IntoIter = std::vec::IntoIter<f32>;
+    
     fn into_iter(self) -> Self::IntoIter {
         self.genes.into_iter()
     }
